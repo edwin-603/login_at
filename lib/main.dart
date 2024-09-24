aa524af18c0d20ceb9b8c42e067576c79966e8f1
edwin-603 <edwinprakash.jesuraj@guruits.com>
2024-09-24 14:35:25 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:login_at/presentation/animations/animation.dart';
+import 'Aadhaar/aadhaar_scan.dart';
+import 'utils/translation.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      translations: AppTranslations(),
+      locale: Locale('en', 'US'),
+      fallbackLocale: Locale('en', 'US'),
+      title: 'Flutter Animated Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //home: AnimatedLoginScreen(),
+      home: AadhaarScanDetailsPage(),
+    );
+  }
+}
